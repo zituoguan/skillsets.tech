5facddcedd3cb41a9022111b30a004b5ceb711cb
stefanicjuraj <jurajstefanic@outlook.com>
2024-09-07 02:22:34 +0200

    refactor(style): remove loading & add animation

--- src/pages/FullStack.tsx
+++ src/pages/FullStack.tsx
@@ -41,7 +41,6 @@ const FullStack = () => {
     >(null);
     const [jobAdsCount, setJobAdsCount] = useState(0);
     const [skillCount, setSkillCount] = useState(0);
-    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         async function init() {
@@ -120,7 +119,6 @@ const FullStack = () => {
                     data: Object.values(techData),
                 },
             ]);
-            setLoading(false);
         }
 
         init();
@@ -165,7 +163,7 @@ const FullStack = () => {
 
     return (
         <div className="app">
-            <header className="text-center pt-40 mb-10 p-4">
+            <header className="text-center pt-40 mb-10 p-4 animation glow delay-1">
                 <p className="text-4xl font-bold mt-10">
                     The most wanted
                     <span className="text-indigo-500"> Full Stack </span>
@@ -175,53 +173,26 @@ const FullStack = () => {
                     Browse skills required for your job position
                 </p>
             </header>
-            {loading && (
-                <div className="mx-auto text-center">
-                    <div className="animate-spin">
-                        <svg
-                            aria-hidden="true"
-                            className="w-12 h-12 mx-auto text-gray-200 text-center animate-spin fill-indigo-500"
-                            viewBox="0 0 100 101"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                                fill="currentColor"
-                            />
-                            <path
-                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                                fill="currentFill"
-                            />
-                        </svg>
-                    </div>
-                    <p className="mt-4 text-gray-500">Loading data...</p>
-                </div>
-            )}
-            {!loading && (
-                <>
-                    <div className="flex flex-col sm:flex-row justify-center items-center mt-20 mb-10">
-                        <div
-                            id="jobAdsCount"
-                            className="text-center text-2xl font-semibold sm:mr-10"
-                        ></div>
-                        <div
-                            id="skillCount"
-                            className="text-center text-2xl font-semibold sm:ml-10 mt-5 sm:mt-0"
-                        ></div>
-                    </div>
-                    <div className="mt-0 mb-40 mx-auto max-w-screen-lg p-4">
-                        {chartOptions && chartSeries && (
-                            <Chart
-                                options={chartOptions}
-                                series={chartSeries}
-                                type="bar"
-                                height="2000"
-                            />
-                        )}
-                    </div>
-                </>
-            )}
+            <div className="flex flex-col sm:flex-row justify-center items-center mt-20 mb-10 animation glow delay-2">
+                <div
+                    id="jobAdsCount"
+                    className="text-center text-2xl font-semibold sm:mr-10"
+                ></div>
+                <div
+                    id="skillCount"
+                    className="text-center text-2xl font-semibold sm:ml-10 mt-5 sm:mt-0"
+                ></div>
+            </div>
+            <div className="mt-0 mb-40 mx-auto max-w-screen-lg p-4 animation glow delay-3">
+                {chartOptions && chartSeries && (
+                    <Chart
+                        options={chartOptions}
+                        series={chartSeries}
+                        type="bar"
+                        height="2000"
+                    />
+                )}
+            </div>
         </div>
     );
 };
