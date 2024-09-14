087f8dd79173487a549494767efeeb73f64ca99c
stefanicjuraj <jurajstefanic@outlook.com>
2024-09-15 00:51:00 +0200

    feat: add seniority button & dropdown

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,57 +4,95 @@ import { Link, useLocation } from "react-router-dom";
 const Navbar: React.FC = () => {
     const location = useLocation();
     const [buttonText, setButtonText] = useState("Job Positions");
-    // const [seniorityButtonText, setSeniorityButtonText] = useState("Seniority");
+    const [seniorityButtonText, setSeniorityButtonText] = useState("Seniority");
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-    // const [isSeniorityDropdownOpen, setIsSeniorityDropdownOpen] =
-    //     useState(false);
+    const [isSeniorityDropdownOpen, setIsSeniorityDropdownOpen] =
+        useState(false);
     const [isNavbarOpen, setIsNavbarOpen] = useState(false);
     const dropdownRef = useRef<HTMLDivElement>(null);
-    // const seniorityDropdownRef = useRef<HTMLDivElement>(null);
+    const seniorityDropdownRef = useRef<HTMLDivElement>(null);
     const buttonRef = useRef<HTMLButtonElement>(null);
-    // const seniorityButtonRef = useRef<HTMLButtonElement>(null);
+    const seniorityButtonRef = useRef<HTMLButtonElement>(null);
 
     useEffect(() => {
-        switch (location.pathname) {
-            case "/frontend":
-                setButtonText("Frontend");
-                break;
-            case "/backend":
-                setButtonText("Backend");
-                break;
-            case "/devops":
-                setButtonText("DevOps");
-                break;
-            case "/data":
-                setButtonText("Data");
-                break;
-            case "/full-stack":
-                setButtonText("Full Stack");
-                break;
-            case "/design":
-                setButtonText("UX / UI");
-                break;
-            case "/quality-assurance":
-                setButtonText("Quality Assurance");
-                break;
-            case "/mobile":
-                setButtonText("iOS/Android");
-                break;
-            case "/management":
-                setButtonText("Management");
-                break;
-            default:
-                setButtonText("Job Positions");
-                break;
+        if (location.pathname === "/") {
+            setButtonText("Job Positions");
+            setSeniorityButtonText("Seniority");
+        } else if (
+            location.pathname.includes("/frontend") ||
+            location.pathname.includes("/backend") ||
+            location.pathname.includes("/devops") ||
+            location.pathname.includes("/data") ||
+            location.pathname.includes("/full-stack") ||
+            location.pathname.includes("/design") ||
+            location.pathname.includes("/quality-assurance") ||
+            location.pathname.includes("/management") ||
+            location.pathname.includes("/mobile")
+        ) {
+            switch (location.pathname) {
+                case "/frontend":
+                    setButtonText("Frontend");
+                    break;
+                case "/backend":
+                    setButtonText("Backend");
+                    break;
+                case "/devops":
+                    setButtonText("DevOps");
+                    break;
+                case "/data":
+                    setButtonText("Data");
+                    break;
+                case "/full-stack":
+                    setButtonText("Full Stack");
+                    break;
+                case "/design":
+                    setButtonText("UX / UI");
+                    break;
+                case "/quality-assurance":
+                    setButtonText("Quality Assurance");
+                    break;
+                case "/management":
+                    setButtonText("Management");
+                    break;
+                case "/mobile":
+                    setButtonText("iOS/Android");
+                    break;
+                default:
+                    setButtonText("Job Positions");
+                    break;
+            }
+            setSeniorityButtonText("Seniority");
+        } else if (
+            location.pathname.includes("/junior") ||
+            location.pathname.includes("/mid") ||
+            location.pathname.includes("/senior")
+        ) {
+            switch (location.pathname) {
+                case "/junior":
+                    setSeniorityButtonText("Junior");
+                    break;
+                case "/mid":
+                    setSeniorityButtonText("Mid");
+                    break;
+                case "/senior":
+                    setSeniorityButtonText("Senior");
+                    break;
+                default:
+                    setSeniorityButtonText("Seniority");
+                    break;
+            }
+            setButtonText("Job Positions");
         }
 
         setIsDropdownOpen(false);
+        setIsSeniorityDropdownOpen(false);
     }, [location.pathname]);
 
     useEffect(() => {
         if (isDropdownOpen && dropdownRef.current && buttonRef.current) {
             const buttonRect = buttonRef.current.getBoundingClientRect();
             dropdownRef.current.style.top = `${buttonRect.bottom}px`;
+
             if (window.innerWidth < 768) {
                 dropdownRef.current.style.left = `50%`;
                 dropdownRef.current.style.transform = `translateX(-50%)`;
@@ -65,37 +103,65 @@ const Navbar: React.FC = () => {
                 dropdownRef.current.style.transform = `translateX(-50%)`;
             }
         }
-    }, [isDropdownOpen]);
+
+        if (
+            isSeniorityDropdownOpen &&
+            seniorityDropdownRef.current &&
+            seniorityButtonRef.current
+        ) {
+            const buttonRect =
+                seniorityButtonRef.current.getBoundingClientRect();
+            seniorityDropdownRef.current.style.top = `${buttonRect.bottom}px`;
+
+            if (window.innerWidth < 768) {
+                seniorityDropdownRef.current.style.left = `50%`;
+                seniorityDropdownRef.current.style.transform = `translateX(-50%)`;
+            } else {
+                seniorityDropdownRef.current.style.left = `${
+                    buttonRect.left + buttonRect.width / 2
+                }px`;
+                seniorityDropdownRef.current.style.transform = `translateX(-50%)`;
+            }
+        }
+    }, [isDropdownOpen, isSeniorityDropdownOpen]);
 
     const toggleDropdown = () => {
         setIsDropdownOpen(!isDropdownOpen);
-        // setIsSeniorityDropdownOpen(false);
+        setIsSeniorityDropdownOpen(false);
     };
 
-    // const toggleSeniorityDropdown = () => {
-    //     setIsSeniorityDropdownOpen(!isSeniorityDropdownOpen);
-    //     setIsDropdownOpen(false);
-    // };
+    const toggleSeniorityDropdown = () => {
+        setIsSeniorityDropdownOpen(!isSeniorityDropdownOpen);
+        setIsDropdownOpen(false);
+    };
 
     const toggleNavbar = () => {
         setIsNavbarOpen(!isNavbarOpen);
     };
 
     return (
-        <nav className="bg-white border-gray-200 fixed top-0 start-0 w-full z-20 sm:py-0 py-3">
+        <nav className="bg-white border-gray-200 fixed top-0 start-0 w-full z-20 sm:py-1 py-3">
             <div className="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4">
                 <Link
                     to="/"
                     className="flex items-center space-x-3 rtl:space-x-reverse"
                 >
-                    <span className="self-center sm:text-5xl text-3xl font-bold whitespace-nowrap">
+                    <span className="self-center sm:text-4xl text-3xl font-bold whitespace-nowrap">
                         Skill<span className="text-indigo-500">Sets</span>
                     </span>
                 </Link>
                 <div className="flex items-center md:order-2 space-x-1 md:space-x-2 rtl:space-x-reverse">
-                    <p className="text-black rounded-lg text-md px-5 invisible">
-                        Contribute
-                    </p>
+                    <a
+                        href="https://github.com/stefanicjuraj/skillsets"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img
+                            src="/assets/icons/github.svg"
+                            alt="chevron"
+                            className="w-8 h-8"
+                        />
+                    </a>
                     <button
                         type="button"
                         className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-black rounded-lg md:hidden focus:outline-none focus:ring-1 focus:ring-indigo-300"
@@ -120,7 +186,7 @@ const Navbar: React.FC = () => {
                                 id="mega-menu-dropdown-button"
                                 ref={buttonRef}
                                 data-dropdown-toggle="mega-menu-dropdown"
-                                className="flex items-center justify-between w-full md:w-auto p-5 rounded text-white border-b bg-indigo-500 relative whitespace-nowrap"
+                                className="flex items-center justify-between w-full md:w-auto p-5 rounded text-white border-b bg-indigo-500 relative whitespace-nowrap rounded-md"
                                 onClick={toggleDropdown}
                                 aria-expanded={
                                     isDropdownOpen ? "true" : "false"
@@ -293,6 +359,79 @@ const Navbar: React.FC = () => {
                                                     className="w-5 h-5 mr-2"
                                                 />
                                                 Management
+                                            </Link>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                    <ul className="flex flex-col mt-4 md:flex-row md:mt-0 md:space-x-8 rtl:space-x-reverse ml-3">
+                        <li className="md:flex md:items-center md:justify-center w-full">
+                            <button
+                                id="seniority-dropdown-button"
+                                ref={seniorityButtonRef}
+                                data-dropdown-toggle="seniority-menu-dropdown"
+                                className="flex items-center justify-between w-full md:w-auto p-5 rounded font-medium text-white border-b bg-indigo-500 relative rounded-md"
+                                onClick={toggleSeniorityDropdown}
+                                aria-expanded={
+                                    isSeniorityDropdownOpen ? "true" : "false"
+                                }
+                            >
+                                {seniorityButtonText}
+                                <img
+                                    src="/assets/icons/chevron.svg"
+                                    alt="chevron"
+                                    className="w-6 h-6 ml-3"
+                                />
+                            </button>
+                            <div
+                                ref={seniorityDropdownRef}
+                                id="seniority-menu-dropdown"
+                                className={`absolute z-10 ${
+                                    isSeniorityDropdownOpen ? "block" : "hidden"
+                                } max-w-sm sm:text-md text-sm bg-white border border-gray-100 rounded-lg shadow-md`}
+                                style={{ minWidth: "200px" }}
+                            >
+                                <div className="p-10">
+                                    <ul className="space-y-7">
+                                        <li>
+                                            <Link
+                                                to="/"
+                                                className="flex items-center text-black hover:text-indigo-500 pointer-events-none"
+                                                onClick={() =>
+                                                    setIsSeniorityDropdownOpen(
+                                                        false
+                                                    )
+                                                }
+                                            >
+                                                Junior
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link
+                                                to="/"
+                                                className="flex items-center text-black hover:text-indigo-500 pointer-events-none"
+                                                onClick={() =>
+                                                    setIsSeniorityDropdownOpen(
+                                                        false
+                                                    )
+                                                }
+                                            >
+                                                Mid
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link
+                                                to="/"
+                                                className="flex items-center text-black hover:text-indigo-500 pointer-events-none"
+                                                onClick={() =>
+                                                    setIsSeniorityDropdownOpen(
+                                                        false
+                                                    )
+                                                }
+                                            >
+                                                Senior
                                             </Link>
                                         </li>
                                     </ul>
