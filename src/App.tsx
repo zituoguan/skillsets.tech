c9f9915402db707780ab5604ed1c57e0904ae43f
stefanicjuraj <jurajstefanic@outlook.com>
2024-09-28 02:49:52 +0200

    feat: add route to trends

--- src/App.tsx
+++ src/App.tsx
@@ -13,30 +13,33 @@ import Management from "./pages/Management";
 import Junior from "./pages/seniority/Junior";
 import Mid from "./pages/seniority/Mid";
 import Senior from "./pages/seniority/Senior";
+import Trends from "./pages/Trends";
 
 function App() {
     return (
-        <>
-            <Router>
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/frontend" element={<Frontend />} />
-                    <Route path="/backend" element={<Backend />} />
-                    <Route path="/devops" element={<DevOps />} />
-                    <Route path="/data" element={<Data />} />
-                    <Route path="/full-stack" element={<FullStack />} />
-                    <Route path="/design" element={<Design />} />
-                    <Route path="/quality-assurance" element={<QualityAssurance />} />
-                    <Route path="/mobile" element={<Mobile />} />
-                    <Route path="/management" element={<Management />} />
-                    {/* seniority */}
-                    <Route path="/junior" element={<Junior />} />
-                    <Route path="/mid" element={<Mid />} />
-                    <Route path="/senior" element={<Senior />} />
-                </Routes>
-            </Router>
-        </>
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                {/* positions */}
+                <Route path="/frontend" element={<Frontend />} />
+                <Route path="/backend" element={<Backend />} />
+                <Route path="/devops" element={<DevOps />} />
+                <Route path="/data" element={<Data />} />
+                <Route path="/full-stack" element={<FullStack />} />
+                <Route path="/design" element={<Design />} />
+                <Route path="/quality-assurance" element={<QualityAssurance />}
+                />
+                <Route path="/mobile" element={<Mobile />} />
+                <Route path="/management" element={<Management />} />
+                {/* seniority */}
+                <Route path="/junior" element={<Junior />} />
+                <Route path="/mid" element={<Mid />} />
+                <Route path="/senior" element={<Senior />} />
+                {/* trends */}
+                <Route path="/trends" element={<Trends />} />
+            </Routes>
+        </Router>
     );
 }
 
