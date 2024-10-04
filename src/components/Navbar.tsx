3a1bc70a1bbd6b0a6fe2aebaf41f8161e2bb23f7
stefanicjuraj <jurajstefanic@outlook.com>
2024-10-04 18:26:43 +0200

    chore: update last updated date

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -163,7 +163,7 @@ const Navbar: React.FC = () => {
                         <p className="mr-2 text-xs text-gray-500 sm:text-sm">
                             Last updated
                             <br />
-                            03/10/2024
+                            04/10/2024
                         </p>
                     </a>
                     <button
