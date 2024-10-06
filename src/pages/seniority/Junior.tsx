8f0c409d955e49ab3c5bcb7a23247fa88ecaec9c
stefanicjuraj <jurajstefanic@outlook.com>
2024-10-06 23:24:41 +0200

    chore(style): modify animation delay

--- src/pages/seniority/Junior.tsx
+++ src/pages/seniority/Junior.tsx
@@ -128,7 +128,7 @@ const Junior = () => {
                     Browse skills required for your job position
                 </h2>
             </header>
-            <div className="flex flex-col items-center justify-center mt-16 mb-16 sm:flex-row animation glow delay-2">
+            <div className="flex flex-col items-center justify-center mt-16 mb-16 sm:flex-row animation glow delay-1">
                 <div
                     id="jobAdsCount"
                     className="text-2xl font-semibold text-center sm:mr-10"
@@ -147,7 +147,7 @@ const Junior = () => {
                     className="w-full max-w-sm p-3 border border-2 border-indigo-300 rounded-lg hover:border-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent"
                 />
             </div>
-            <div className="mx-auto text-center animation glow delay-3">
+            <div className="mx-auto text-center animation glow delay-1">
                 <p className="w-full mx-auto text-xs text-gray-500 px-8">
                     Search multiple skills by separating them with a space
                 </p>
@@ -163,7 +163,7 @@ const Junior = () => {
                     />
                 </button>
             </div>
-            <div className="grid max-w-screen-sm grid-cols-1 gap-4 px-4 mx-auto mt-8 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 mb-80 animation glow delay-3">
+            <div className="grid max-w-screen-sm grid-cols-1 gap-4 px-4 mx-auto mt-8 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 mb-80 animation glow delay-1">
                 {filteredSkills.map(([skill, count]) => (
                     <div
                         key={skill}
