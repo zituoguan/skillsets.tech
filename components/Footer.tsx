c33344b2552cabcadbb8028d9663b429b02c275c
Juraj Stefanic <jurajstefanic@outlook.com>
2025-02-18 16:43:43 +0100

    chore: update footer animation delay

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="max-w-screen-lg mx-auto py-5 bg-[rgba(11,11,11,0.8)] backdrop-blur-lg rounded-t animation glow delay-2">
+    <footer className="max-w-screen-lg mx-auto py-5 bg-[rgba(11,11,11,0.8)] backdrop-blur-lg rounded-t animation glow delay-3">
       <div className="flex justify-around items-center text-white text-sm max-w-screen-md mx-auto">
         <Link
           href="/about"
