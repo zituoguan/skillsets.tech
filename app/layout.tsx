a2ee5cde7c3dd1db772b4d2d7e72480b72351090
Juraj Stefanic <jurajstefanic@outlook.com>
2025-02-18 20:00:45 +0100

    feat: add google tag manager scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,12 +37,29 @@ export default function RootLayout({
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/assets/favicon.svg" sizes="any" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-562CWBQ2');`,
+          }}
+        />
         <Script
           src="https://unpkg.com/@tailwindcss/browser@4"
           strategy="beforeInteractive"
         />
       </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-562CWBQ2"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Navbar />
         {children}
         <Footer />
